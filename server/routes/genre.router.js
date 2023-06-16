5d2ed0ec5c4b77829569effb43664f28a8d41763
Zackaria Antar <antarzack@gmail.com>
2023-06-16 11:14:06 -0500

    set up project and built genre router code.

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
+  const genreQuery =  `SELECT * FROM genres ORDER BY name ASC;`
+
+  pool.query(genreQuery).then((result)=>{
+    console.log('GENRE GET REQUEST SUCCESS');
+    res.send(result.rows)
+
+  }).catch((err)=>{
+    console.log("ERROR WITH GENRE GET", err);
+    res.sendStatus(500)
+
+  })
   // Add query to get all genres
-  res.sendStatus(500)
 });
 
 module.exports = router;
