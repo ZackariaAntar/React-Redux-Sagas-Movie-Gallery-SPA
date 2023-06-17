5c5a7bddee9b4e41c2cb5e2a7fc90879bd3ec189
Zackaria Antar <antarzack@gmail.com>
2023-06-17 13:10:52 -0500

    added mui Grid and Grid item to movie list page.

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import { Link} from "react-router-dom";
-import {Container, Box} from '@mui/material'
+import {Container, Grid} from '@mui/material'
+
 
 function MovieList() {
     const dispatch = useDispatch();
@@ -22,6 +23,12 @@ function MovieList() {
 		<main>
 			<h1 className="page-title">Movie List</h1>
 			<Container maxWidth="lg">
+				<Grid spacing={3}>
+					<Grid item xs>
+						<p>xs</p>
+					</Grid>
+
+				</Grid>
 				{movies.map((movie) => {
 					return (
 						<div key={movie.id} onClick={() => goToDetails(movie)}>
