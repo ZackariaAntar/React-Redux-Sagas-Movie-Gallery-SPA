5d529b25e730f012bcc7d4cba9ce7d49b034e150
Zackaria Antar <antarzack@gmail.com>
2023-06-17 13:45:52 -0500

    added mui cards for the movie posters on movie list page.

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import { Link} from "react-router-dom";
-import {Container, Grid} from '@mui/material'
+import {Container, Grid, Card, CardContent, CardMedia, Typography} from '@mui/material'
 
 
 function MovieList() {
@@ -22,12 +22,37 @@ function MovieList() {
     return (
 		<main>
 			<h1 className="page-title">Movie List</h1>
-			<Container maxWidth="lg">
-				<Grid spacing={3}>
-					<Grid item xs>
-						<p>xs</p>
-					</Grid>
-
+			<Container maxWidth="xl">
+				<Grid container spacing={5}>
+					{movies.map((movie) => (
+						<Grid item xs={3} key={movie.id}>
+							<Card
+								raised
+								sx={{
+									margin: "0 auto",
+									padding: 0.5,
+								}}
+							>
+								<Typography
+									gutterBottom
+									variant="h5"
+									component="div"
+								>
+									{movie.title}
+								</Typography>
+								<CardMedia
+									component="img"
+									sx={{
+										height: 375,
+										objectFit: "contain",
+									}}
+									image={movie.poster}
+									title={movie.title}
+								/>
+								<CardContent></CardContent>
+							</Card>
+						</Grid>
+					))}
 				</Grid>
 				{movies.map((movie) => {
 					return (
