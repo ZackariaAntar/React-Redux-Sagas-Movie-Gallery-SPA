7dbfe4349758a8b0835d1a64d7287477c89a1177
Zackaria Antar <antarzack@gmail.com>
2023-06-18 10:47:32 -0500

    added comments to the movie router module.

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
+
+// endpoint from fetchAllMovies axios request
+// requests all data from the movies table in the database
+// returns data as an array of objects to the fetchAllMovies Saga
 router.get('/', (req, res) => {
 
   const query = `SELECT * FROM movies ORDER BY "title" ASC`;
@@ -16,56 +20,44 @@ router.get('/', (req, res) => {
     })
 
 });
-router.get('/only/:id', (req, res) => {
-  const filmId = req.params.id
-  const query = `SELECT * FROM movies WHERE id = $1 ASC`;
-  pool.query(query, [filmId])
-    .then( result => {
-      console.log('GET ALL MOVIES ORDERED BY TITLE SUCCESS');
-      res.send(result.rows);
-    })
-    .catch(err => {
-      console.log('ERROR: Get all movies', err);
-      res.sendStatus(500)
-    })
 
-});
+//======= POST TO BE USED IN A FUTURE FEATURE BRANCH ========//
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  // RETURNING "id" will give us back the id of the created movie
-  const insertMovieQuery = `
-  INSERT INTO "movies" ("title", "poster", "description")
-  VALUES ($1, $2, $3)
-  RETURNING "id";`
+// router.post('/', (req, res) => {
+//   console.log(req.body);
+//   // RETURNING "id" will give us back the id of the created movie
+//   const insertMovieQuery = `
+//   INSERT INTO "movies" ("title", "poster", "description")
+//   VALUES ($1, $2, $3)
+//   RETURNING "id";`
 
-  // FIRST QUERY MAKES MOVIE
-  pool.query(insertMovieQuery, [req.body.title, req.body.poster, req.body.description])
-  .then(result => {
-    console.log('New Movie Id:', result.rows[0].id); //ID IS HERE!
+//   // FIRST QUERY MAKES MOVIE
+//   pool.query(insertMovieQuery, [req.body.title, req.body.poster, req.body.description])
+//   .then(result => {
+//     console.log('New Movie Id:', result.rows[0].id); //ID IS HERE!
 
-    const createdMovieId = result.rows[0].id
+//     const createdMovieId = result.rows[0].id
 
-    // Now handle the genre reference
-    const insertMovieGenreQuery = `
-      INSERT INTO "movies_genres" ("movie_id", "genre_id")
-      VALUES  ($1, $2);
-      `
-      // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
-      pool.query(insertMovieGenreQuery, [createdMovieId, req.body.genre_id]).then(result => {
-        //Now that both are done, send back success!
-        res.sendStatus(201);
-      }).catch(err => {
-        // catch for second query
-        console.log(err);
-        res.sendStatus(500)
-      })
+//     // Now handle the genre reference
+//     const insertMovieGenreQuery = `
+//       INSERT INTO "movies_genres" ("movie_id", "genre_id")
+//       VALUES  ($1, $2);
+//       `
+//       // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
+//       pool.query(insertMovieGenreQuery, [createdMovieId, req.body.genre_id]).then(result => {
+//         //Now that both are done, send back success!
+//         res.sendStatus(201);
+//       }).catch(err => {
+//         // catch for second query
+//         console.log(err);
+//         res.sendStatus(500)
+//       })
 
-// Catch for first query
-  }).catch(err => {
-    console.log(err);
-    res.sendStatus(500)
-  })
-})
+// // Catch for first query
+//   }).catch(err => {
+//     console.log(err);
+//     res.sendStatus(500)
+//   })
+// })
 
 module.exports = router;
